1e424eba2c0d760c29bf142ac5b3b61ee3b44bd5
duytai1998 <tainhse05780@fpt.edu.vn>
2020-07-15 13:29:47 +0700

    first try upload course image to azure

--- config/routes.rb
+++ config/routes.rb
@@ -12,21 +12,21 @@ Rails.application.routes.draw do
       unlock: 'unblock',
       registration: 'register',
       sign_up: 'sign_up'}
-  #
-  # get 'home', to: 'courses#index'
-  # resources :courses, only: [:index, :show] do
-  #   resources :articles, only: [:index, :show]
-  # end
-  # resources :articles , only: [:index, :show]do
-  #   resources :articles, only: [:index, :show]
-  #
-  # end
-  #
-  #
-  # namespace :admin do
-  #   resources :users, except: :delete
-  #   resources :courses, except: [:create, :delete]
-  #   resources :articles , except: [:create, :delete]
-  #
-  # end
+
+  get 'home', to: 'courses#index'
+  resources :courses, only: [:index, :show] do
+    resources :articles, only: [:index, :show]
+  end
+  resources :articles , only: [:index, :show]do
+    resources :articles, only: [:index, :show]
+
+  end
+
+
+  namespace :admin do
+    resources :users, except: :delete
+    resources :courses, except: [:create, :delete]
+    resources :articles , except: [:create, :delete]
+
+  end
 end
