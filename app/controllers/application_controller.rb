24e74efc522eb4cbdf0d903d7084e7d9f20d501b
duytai1998 <tainhse05780@fpt.edu.vn>
2020-07-26 22:03:38 +0700

    update view layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
   def left_side_data
     if !current_user.present?
-      data = Topic.select(:name, :id).limit(5)
+      @topics = Topic.select(:name, :id).limit(5)
     else
       @my_courses = Course.select(:name, :id).where('customer_id = ?', current_user.id).limit(5)
     end
