45e5d9a7d2bd0c92f84ac72a6ac1389b05cd823c
HoanAlias <trinhthehoan989@gmail.com>
2020-07-27 23:32:03 +0700

    add filter on search page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, :left_side_data
+  before_action  :left_side_data
 
   private
   def left_side_data
     if !current_user.present?
-      data = Topic.select(:name, :id).limit(5)
+      @data = Topic.select(:name, :id).limit(5)
     else
       @my_courses = Course.select(:name, :id).where('customer_id = ?', current_user.id).limit(5)
     end
