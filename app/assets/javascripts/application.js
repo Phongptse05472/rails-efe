723150bc8638aeca84addbeb0b88a918f93bf5ad
HoanAlias <trinhthehoan989@gmail.com>
2020-08-02 23:20:10 +0700

    update arichived icon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,22 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-//= require rails-ujs
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
+require("mailers")
+require("jquery")
+require("jquery-ui")
+require('home')
+
+
+
+//= require_tree .
+//= require_self
+//= require home
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
