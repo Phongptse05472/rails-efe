b9e12d46f673741065c6c0c0d96b49476ec519ee
duytai1998 <tainhse05780@fpt.edu.vn>
2020-08-13 17:44:17 +0700

    create customer when register new account

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -12,13 +12,12 @@ class Users::SessionsController < Devise::SessionsController
   # POST /resource/sign_in
   def create
     super
-    # @current_user_id = current_user.id
-    # @customer = Customer.where(user_id: @current_user_id, role_id: 3)
-    # if !@customer.exists?
-    # else
-    #   @customer = Customer.create(id: @current_user_id ,user_id: @current_user_id, role_id: 3)
-    #
-    # end
+    @current_user_id = current_user.id
+    @customer = Customer.where(user_id: @current_user_id)
+    if !@customer.exists?
+      @customer = Customer.create(user_id: @current_user_id, role_id: 3)
+    else
+    end
   end
 
   # DELETE /resource/sign_out
