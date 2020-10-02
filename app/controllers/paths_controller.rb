886c4ef6d475956083d5af543e76e930a27291ba
HoanAlias <trinhthehoan989@gmail.com>
2020-10-02 22:00:59 +0700

    update popup

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -3,12 +3,26 @@ class PathsController < ApplicationController
   def index
     @path = params[:careerpath_id]
     @option = params[:careerpath_id]
+    @current_path_id = CustomersPath.where("customer_id = ?", current_user.id)
+    @current_path = Careerpath.where("id = ?" , @current_path_id.first.careerpath_id)
+    if @option.nil?
+      @option = @current_path_id.first.careerpath_id
+      @path = @option
+    else
+    end
+    # course_lo_str = Course.where('courses.id = ?', @course.id)
+    # @course_lo_arr = course_lo_str.first.lo.tr('', '').split(';').map(&:to_i)
+    # @list_lo = Lo.where("id IN (?)", @course_lo_arr).group(:id)
+    # @skill = Skill.where("id IN (?)", @list_lo.pluck(:skill_id).uniq)
+    # lo_req = LoLo.where("lo_id IN (?)", @course_lo_arr)
+    # req = lo_req.pluck(:lo_req_id).uniq
+    # @lo_req = Lo.where("id IN (?)", req)
+
+
     @list_lo_in_career_path = Lo.joins(:path_los).where("careerpath_id = ?", @path)
     @customer_lo_owner = Lo.joins(:customer_los, :path_los).where("customer_id = ? AND careerpath_id = ?", current_user.id, @path)
     @list_lo_not_owner = @list_lo_in_career_path - @customer_lo_owner
-
-    @current_path_id = CustomersPath.where("customer_id = ?", current_user.id)
-    @current_path = Careerpath.where("id = ?" , @current_path_id.first.careerpath_id)
+    # @lo_req = Lo.where("id IN (?)", @list_lo_in_career_path)
   end
 
   def new
