56f3cb69d7fcab848dd6a99283bd95c52f7c8bc6
duytai1998 <tai9dltt@gmail.com>
2020-10-03 15:14:40 +0700

    insert Lo + lo_req when check in change path

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -4,25 +4,17 @@ class PathsController < ApplicationController
     @path = params[:careerpath_id]
     @option = params[:careerpath_id]
     @current_path_id = CustomersPath.where("customer_id = ?", current_user.id)
-    @current_path = Careerpath.where("id = ?" , @current_path_id.first.careerpath_id)
+    @current_path = Careerpath.where("id = ?", @current_path_id.first.careerpath_id)
     if @option.nil?
       @option = @current_path_id.first.careerpath_id
       @path = @option
     else
     end
-    # course_lo_str = Course.where('courses.id = ?', @course.id)
-    # @course_lo_arr = course_lo_str.first.lo.tr('', '').split(';').map(&:to_i)
-    # @list_lo = Lo.where("id IN (?)", @course_lo_arr).group(:id)
-    # @skill = Skill.where("id IN (?)", @list_lo.pluck(:skill_id).uniq)
-    # lo_req = LoLo.where("lo_id IN (?)", @course_lo_arr)
-    # req = lo_req.pluck(:lo_req_id).uniq
-    # @lo_req = Lo.where("id IN (?)", req)
-
-
     @list_lo_in_career_path = Lo.joins(:path_los).where("careerpath_id = ?", @path)
     @customer_lo_owner = Lo.joins(:customer_los, :path_los).where("customer_id = ? AND careerpath_id = ?", current_user.id, @path)
+
     @list_lo_not_owner = @list_lo_in_career_path - @customer_lo_owner
-    # @lo_req = Lo.where("id IN (?)", @list_lo_in_career_path)
+
   end
 
   def new
@@ -41,7 +33,12 @@ class PathsController < ApplicationController
       @cus_path = CustomersPath.where("customer_id = ?", current_user.id)
       @cus_path.update(customer_id: current_user.id, careerpath_id: path_id)
       @customer_lo = CustomerLo.new
-      list_lo_checked.each do |lo_id|
+      lo_req = LoLo.where("lo_id IN (?)", list_lo_checked)
+      arr_lo_req = lo_req.pluck(:lo_req_id)
+      list_lo_checked = list_lo_checked + arr_lo_req
+      valid_list_insert = list_lo_checked.map(&:to_i).uniq
+      binding.pry
+      valid_list_insert.each do |lo_id|
         CustomerLo.find_or_create_by(customer_id: current_user.id, lo_id: lo_id)
         if lo_id.equal?(list_lo_checked.last)
           redirect_to user_home_path
