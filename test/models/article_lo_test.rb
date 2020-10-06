a1a74f45584af33499591c2d3b481f0abe2ca1e7
duytai1998 <tai9dltt@gmail.com>
2020-10-06 08:36:56 +0700

    add temp

---
+++ test/models/article_lo_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ArticleLoTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/paths_lo_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PathsLoTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
